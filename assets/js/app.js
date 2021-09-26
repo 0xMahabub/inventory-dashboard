33d8b25d33294fcc8a03e90f095f640f24c143b6
Md Mahabub Alam <mahabub212002072@gmail.com>
2021-09-26 13:53:29 +0600

    navbar staged

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Javascript for functional UI
+ * Author: Mahabub (mahabub72)
+ */
+
+// working elements
+var usrBtn = document.getElementById('usr_btn');
+var usrMenu = document.getElementById('usr_menu');
+
+// toggle usrMenu
+usrBtn.addEventListener('click', function(){
+    usrMenu.classList.toggle('hidden');
+});
